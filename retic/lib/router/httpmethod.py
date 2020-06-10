e7fa170824d14347fa90432c20cf7d02c465d4fe
B <Florian>
2020-06-10 14:19:46 -0600

    + test for define .default (http methos) return self

--- retic/lib/router/httpmethod.py
+++ retic/lib/router/httpmethod.py
@@ -27,17 +27,12 @@ class HttpMethod(object):
         :param method: HTTP Request Method from the client request
         :param route: Route function from Router class
         """
-        try:
-            if len(args) < 2:
-                raise Exception(
-                    "error: The route has the next format: METHOD(path, [...handlers functions])"
-                )
-            path = args[0]
-            if type(path) is not str:
-                raise TypeError("error: The path type must be a string format")
-            self.route(path, self.method.upper(), args[1:])
-            return self.router
-        except Exception as e:
-            traceback.print_exc(file=sys.stdout)
-            print(get_file_error_exception())
-            sys.exit(0)
+        if len(args) < 2:
+            raise ValueError(
+                "error: The route has the next format: METHOD(path, [...handlers functions])"
+            )
+        path = args[0]
+        if type(path) is not str:
+            raise TypeError("error: The path type must be a string format")
+        self.route(path, self.method.upper(), args[1:])
+        return self.router
